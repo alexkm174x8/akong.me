8b0a6e216c3ed76f7e8ed5af3a3aba30e9c217e4
Alejandro Kong <115722702+alexkm174x8@users.noreply.github.com>
2024-08-14 13:41:16 -0600

    Perfected nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { nav } from '@/data';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import menu from '@/public/svg/menu'
 import MenuSvg from '@/public/svg/menu';
+import { enablePageScroll, disablePageScroll } from 'scroll-lock';
 
 
 
@@ -13,22 +14,41 @@ const Navbar = () => {
     const toggleNav = () => {
         if(openNav){
             setOpenNav(false);
+            enablePageScroll();
         }else{
             setOpenNav(true);
+            disablePageScroll();
         }
     };
 
     const handleClick = () => {
+        if(!openNav) return;
+
+        enablePageScroll();
         setOpenNav(false);
     }
 
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 1024) {
+                setOpenNav(false);
+                enablePageScroll();
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <nav className='sticky flex max-w-fit top-[2vh] inset-x-0 mx-auto px-5 py-2 items-center justify-center space-x-4 z-50 bg-black backdrop-filter backdrop-blur-lg bg-opacity-30 rounded-3xl text-gray border border-black border-opacity-10'>
-            <div className='relative flex flex-col items-center justify-center lg:flex-row gap-10'>
+        <nav className='sticky flex top-[2vh] inset-x-0 mx-auto items-center justify-end lg:justify-center space-x-4 z-50 lg:w-fit'>
+            <div className={`relative flex flex-col items-end justify-center lg:flex-row gap-10 bg-black backdrop-filter backdrop-blur-lg bg-opacity-30 rounded-3xl text-gray border border-black border-opacity-10 px-5 py-2 ${openNav ? 'w-[67dvh] h-[95dvh]' : ''}`}>
                 <button className='mb-auto flex lg:hidden' onClick={toggleNav}>
                     <MenuSvg openNav={openNav}/>
                 </button>
-                <div className={`static lg:flex flex-col items-center justify-center m-auto lg:flex-row lg:gap-10 max-md:my-10 ${openNav ? 'flex gap-10 w-[100dvw] h-[100dvh]' : 'hidden'}`}>
+                <div className={`static lg:flex flex-col items-center justify-center m-auto lg:flex-row lg:gap-10 max-md:my-10 w-[100%] ${openNav ? 'flex gap-20 w-[100dvw] h-[100dvh]' : 'hidden'}`}>
                     {nav.map((item) => (
                         <a
                             key = {item.id}
@@ -37,7 +57,7 @@ const Navbar = () => {
                             className={`relative w-fit lg:block after:block after:content-[""] after:absolute after:h-[2px] after:bg-white after:rounded-full after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center hover:text-white hover:duration-700`}
                         >
 
-                            {item.id === '2' ? <img src={item.img} alt="Logo" className='hidden lg:flex bg-clip-content'/> : `${item.title}`}
+                            {item.id === '2' ? <img src={item.img} alt="Logo" className='hidden lg:flex'/> : `${item.title}`}
                             {/* {item.img ? <img src={item.img} alt="Logo" className='sm:hidden' /> : `${item.title}`}  */}
                         </a>
                     ))}
