2234f31fe3150fcf109ebcbdbe6d179504ccda90
alexkm174x8 <akongmontoya@hotmail.com>
2025-02-17 17:05:36 -0600

    Updated recent projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,15 @@
 "use client";
 import React from "react";
 import Card from "./ui/card"
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalTrigger,
+} from "@/components/ui/animated-modal";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 
 const Projects = () => {
@@ -15,10 +24,11 @@ const Projects = () => {
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_1%,black)]">
             </div>
             
-            <div className=" flex flex-col lg:flex-row lg:space-x-32 px-8 justify-center mb-30">
-                <Card title="Topos FC Website" description="Worked with Topos FC association to implement new functions to their site" img="/topos.png" link="https://github.com/alexkm174x8/topos-fc"/>
-
-                <Card title="Chase: Take a chance" description="Built a simple game, where a spider comes for you while you need to collect some coins to save yourself " img="/chase.png" link="https://github.com/CharismaticTopHat/chase-take-a-chance"/>
+            <div className="flex flex-col">
+                <div className="flex flex-col lg:flex-row lg:space-x-32 px-8 justify-center">
+                    <Card title="Tentzo App" description="Developed an app that promotes biocultural knowledge with plant identification via camera, a local plant library, and regional activities." img="/oco.png" link="https://github.com/alexkm174x8/Tentzo-App"/>
+                    <Card title="Topos FC Website" description="Worked with Topos FC association to redesign and implement the reservation and tournaments systems for the user and admins." img="/topos.png" link="https://github.com/alexkm174x8/topos-fc"/>
+                </div>             
             </div>
         </div>
     </div>
