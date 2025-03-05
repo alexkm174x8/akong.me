2ba52c29494be77c3f78ab328cd0c33deb14885a
alexkm174x8 <akongmontoya@hotmail.com>
2025-03-05 12:34:11 -0600

    Added name for folders

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Navbar from '@/components/Navbar'
 import React from 'react'
-import Folder from '@/components/ui/Components/Folder/Folder'
+import Folder from '@/components/ui/Folder'
 
 
 export default function ProjectsPage() {
@@ -10,13 +10,13 @@ export default function ProjectsPage() {
       <div className="max-w-7xl w-full">
         <Navbar/>
         <div style={{ 
-          height: '600px', 
+          height: '750px', 
           position: 'relative',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '200px',
-          flexWrap: 'wrap'
+          display: 'grid',
+          gridTemplateColumns: 'repeat(4, 1fr)',
+          gap: '20px',
+          padding: '20px',
+          marginTop: '250px'
         }}>
           <div className="flex flex-col items-center gap-16">
             <Folder 
@@ -24,7 +24,7 @@ export default function ProjectsPage() {
               color="#61A3EB" 
               className="custom-folder" 
             />
-            <span className="text-white text-lg">Project 1</span>
+            <span className="text-white text-lg">Beautiful Patterns Registration</span>
           </div>
           <div className="flex flex-col items-center gap-16">
             <Folder 
@@ -32,7 +32,7 @@ export default function ProjectsPage() {
               color="#61A3EB" 
               className="custom-folder" 
             />
-            <span className="text-white text-lg">Project 1</span>
+            <span className="text-white text-lg">Tentzo App</span>
           </div>
           <div className="flex flex-col items-center gap-16">
             <Folder 
@@ -40,7 +40,7 @@ export default function ProjectsPage() {
               color="#61A3EB" 
               className="custom-folder" 
             />
-            <span className="text-white text-lg">Project 1</span>
+            <span className="text-white text-lg">Educational Platform for CVA</span>
           </div>
           <div className="flex flex-col items-center gap-16">
             <Folder 
@@ -48,7 +48,31 @@ export default function ProjectsPage() {
               color="#61A3EB" 
               className="custom-folder" 
             />
-            <span className="text-white text-lg">Project 1</span>
+            <span className="text-white text-lg">Warehouse Simulation for Cargu</span>
+          </div>
+          <div className="flex flex-col items-center gap-16">
+            <Folder 
+              size={2} 
+              color="#61A3EB" 
+              className="custom-folder" 
+            />
+            <span className="text-white text-lg">Plagarism Detector</span>
+          </div>
+          <div className="flex flex-col items-center gap-16">
+            <Folder 
+              size={2} 
+              color="#61A3EB" 
+              className="custom-folder" 
+            />
+            <span className="text-white text-lg">Traffic Simulator</span>
+          </div>
+          <div className="flex flex-col items-center gap-16">
+            <Folder 
+              size={2} 
+              color="#61A3EB" 
+              className="custom-folder" 
+            />
+            <span className="text-white text-lg">Topos FC Website</span>
           </div>
         </div>
       </div>
